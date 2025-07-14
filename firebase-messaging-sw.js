4e450148c718e853df0c24e9ee1e393b912c2902
Lukman <lukmannrhkm80@gmail.com>
2025-07-14 10:15:35 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,7 +30,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/icons/icon-192x192.png", // Path ke ikon aplikasi Anda
+    icon: "icon-192x192.png", // Path ke ikon aplikasi Anda
   };
 
   // Menampilkan notifikasi ke pengguna.
